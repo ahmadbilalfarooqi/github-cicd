a2490281047b0c322b941385a6abb37a01090d11
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2024-07-04 19:53:59 +0500

    c4

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ def read_root():
     return {"Hello": "World1", }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/")
+def read_root1():
+    return {"Hello": "World2", }
