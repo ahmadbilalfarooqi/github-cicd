92a6a1b991594a33a9eaab50c276ebf3a6069759
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2024-07-04 20:20:15 +0500

    c3

--- app/main.py
+++ app/main.py
@@ -5,9 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1", }
-
-
-@app.get("/")
-def read_root1():
-    return {"Hello": "World2", }
+    return {"Hello": "World1", "message": "Hello World"}
